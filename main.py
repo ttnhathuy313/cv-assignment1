52bc2f576382d083c09bfc07cac3146d2d4b1eab
ttnhuy313 <39652384+ttnhuy313@users.noreply.github.com>
2023-04-08 00:30:18 +0700

    stable lane

--- main.py
+++ main.py
@@ -17,9 +17,16 @@ def draw_parabol(img, parabol, color = [0, 255, 0], thickness = 2, grain=5):
     y = parabol(x)
     points = np.column_stack((y, x)).astype(np.int32)
     cv2.polylines(img, [points], False, color, thickness)
+    
+def Filter(img, length=10):
+    res = img.copy()
+    res = cv2.erode(res, np.ones((length, length), np.uint8))
+    res = cv2.dilate(res, np.ones((length, length), np.uint8))
+    return res
 
 
 def process(img, width = 3):
+    img = cv2.resize(img, (640, 400))
     mxHeight = img.shape[0]
     mxWidth = img.shape[1]
     img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
@@ -37,9 +44,11 @@ def process(img, width = 3):
     roi_mask = np.zeros_like(img_gray)
     roi = cv2.fillPoly(roi_mask, roi_vertices, ignore_mask_color)
     img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    img_threshold = cv2.inRange(img_gray, 180, 255)
+    img_threshold = Filter(cv2.inRange(img_gray, 160, 255))
     img_threshold_isolated = cv2.bitwise_and(img_threshold, roi)
-    img_blur = cv2.GaussianBlur(img_threshold_isolated, (11, 11), 15, 15)
+    # plt.imshow(img_threshold_isolated, cmap = 'gray')
+    # plt.show()
+    img_blur = img_threshold_isolated
     img_edges = cv2.Canny(img_blur, 150, 220)
 
     rho = 1
@@ -53,35 +62,116 @@ def process(img, width = 3):
 
     hough = np.zeros((img_edges.shape[0], img_edges.shape[1], 3), dtype = np.uint8)
     draw_lines(hough, lines)
+    cv2.imshow("img2", hough)
+    cv2.waitKey(1)
+
+    # plt.imshow(hough, cmap = 'gray')
+    # plt.show()
 
     x1 = []
     y1 = []
-    for i in range (mxHeight >> 1, mxHeight - 20, 10):
-        if (i > mxHeight): break
+    minY = mxWidth
+    id = -1
+    allowed_width = 15
+    for i in range (mxHeight >> 1, mxHeight - 20, 5):
         for j in range(0, mxWidth >> 1):
-            if (j > mxWidth): break
             if hough[i, j, 0] == 255:
+                if (j < minY):
+                    minY = j
+                    id = i
+                break
+        if (minY <= 10):
+            break
+
+    lst = minY
+    wo = 0
+    for i in range(id, mxHeight - 20, 2):
+        found = False
+        for j in range(0, mxWidth >> 1):
+            if (hough[i, j, 0] == 255 and abs(j - lst) < allowed_width):
                 x1.append(i)
                 y1.append(j)
+                # hough = cv2.circle(hough, (j, i), 2, (0, 0, 255), 2)
+                lst = j
+                found = True
                 break
+        if (not found):
+            wo += 1
+        else:
+            wo = 0
+        if (wo> 5):
+            break
+    lst = minY
+    wo = 0
+    for i in range(id, mxHeight >> 1, -5):
+        found = False
+        for j in range(0, mxWidth >> 1):
+            if (hough[i, j, 0] == 255 and abs(j - lst) < allowed_width):
+                x1.append(i)
+                y1.append(j)
+                # hough = cv2.circle(hough, (j, i), 2, (0, 0, 255), 2)
+                lst = j
+                found = True
+                break
+
+    # print(x1, y1)
 
     try:
         left_curve  = np.poly1d(np.polyfit(x1,y1, 2))
     except: return img
     res = img.copy()
     draw_parabol(res, left_curve)
-
+    
+    maxY = 0
+    id = -1
+    allowed_width = 15
+    for i in range (mxHeight >> 1, mxHeight - 20, 5):
+        for j in range(mxWidth - 1, mxWidth >> 1, -1):
+            if hough[i, j, 0] == 255:
+                if (j > maxY):
+                    maxY = j
+                    id = i
+                break
+        if (maxY >= mxWidth - 10):
+            break
+    
     x1 = []
     y1 = []
-    for i in range (mxHeight >> 1, mxHeight - 20, 10):
-        if (i > mxHeight): break
+    
+    lst = maxY
+    wo = 0
+    for i in range(id, mxHeight - 20, 2):
+        found = False
         for j in range(mxWidth - 1, mxWidth >> 1, -1):
-            if (j > mxWidth): break
-            if hough[i, j, 0] == 255:
+            if (hough[i, j, 0] == 255 and abs(j - lst) < allowed_width):
                 x1.append(i)
                 y1.append(j)
+                # hough = cv2.circle(hough, (j, i), 2, (0, 0, 255), 2)
+                lst = j
+                found = True
+                break
+        if (not found):
+            wo += 1
+        else:
+            wo = 0
+        if (wo> 5):
+            break
+    lst = maxY
+    wo = 0
+    for i in range(id, mxHeight >> 1, -2):
+        found = False
+        for j in range(mxWidth - 1, mxWidth >> 1, -1):
+            if (j > mxWidth): break
+            if (hough[i, j, 0] == 255 and abs(j - lst) < allowed_width):
+                x1.append(i)
+                y1.append(j)
+                # hough = cv2.circle(hough, (j, i), 2, (0, 0, 255), 2)
+                lst = j
+                found = True
                 break
 
+    # plt.imshow(hough)
+    # plt.show()
     try:
         right_curve = np.poly1d(np.polyfit(x1,y1, 2))
     except: return img
@@ -97,7 +187,6 @@ def process(img, width = 3):
     
     return res 
 
-#400, 640
 
 import yaml
 from yaml.loader import SafeLoader
@@ -106,7 +195,7 @@ with open('cam.yaml') as f:
 
 coeff = 3.858038022 / (data['camera_matrix'][0][0] / 3.858038022535227)
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('./sample/sample3.mp4')
 print('connected')
 print(cap)
 while (True):
@@ -116,6 +205,8 @@ while (True):
     cv2.imshow("img", process(img))
     cv2.waitKey(1)
     
-cap = cv2.VideoCapture('two_lanes.mp4')
-cap.set(cv2.CAP_PROP_POS_MSEC,12000) 
-ret, img = cap.read()
+# cap = cv2.VideoCapture('./sample/sample3.mp4')
+# cap.set(cv2.CAP_PROP_POS_MSEC,6000) 
+# ret, img = cap.read()
+# plt.imshow(process(img))
+# plt.show()
